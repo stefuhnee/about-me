be9e49f6e1468884285b56ee1945a0a9d5b38ab0
Stefanie Hansen <stefuhnee@gmail.com>
2016-03-16 10:19:09 -0700

    Added if/else statements to each question.

--- app.js
+++ app.js
@@ -6,30 +6,64 @@ alert('Welcome to my webpage, ' + welcomeName + '! I\'m going to ask you a few q
 /* Asks user to answer and changes answer to uppercase, storing it in the variable questionOneUser */
 var questionOneUser = prompt('My favorite food is sushi. (y/n)').toUpperCase();
 console.log('Answer: ' + questionOneUser);
+var questionOneAnswer = 'Y';
+
+if (questionOneUser === questionOneAnswer) {
+  alert('Great job! You guessed correctly!');
+}
+else {
+  alert('Nope, that\'s not right.');
+}
 
 var questionTwoUser = prompt('My favorite color is orange. (y/n)').toUpperCase();
 console.log('Answer: ' + questionTwoUser);
+var questionTwoAnswer = 'N';
+
+if (questionTwoUser === questionTwoAnswer) {
+  alert('Great job! You guessed correctly!');
+}
+else {
+  alert('Nope, that\'s not right.');
+}
 
 var questionThreeUser = prompt('I am a unicorn (y/n)').toUpperCase();
 console.log('Answer: ' + questionThreeUser);
+var questionThreeAnswer = 'Y';
+
+if (questionThreeUser === questionThreeAnswer) {
+  alert('Great job! You guessed correctly!');
+}
+else {
+  alert('Nope, that\'s not right.');
+}
 
 var questionFourUser = prompt('I was born in Seattle. (y/n)').toUpperCase();
 console.log('Answer: ' + questionFourUser);
+var questionFourAnswer = 'Y';
+
+if (questionFourUser === questionFourAnswer) {
+  alert('Great job! You guessed correctly!');
+}
+else {
+  alert('Nope, that\'s not right.');
+}
 
 var questionFiveUser = prompt('I enjoy long walks on the beach. (y/n)').toUpperCase();
 console.log('Answer: ' + questionFiveUser);
+var questionFiveAnswer = 'Y';
+
+if (questionFiveUser === questionFiveAnswer) {
+  alert('Great job! You guessed correctly!');
+}
+else {
+  alert('Nope, that\'s not right.');
+}
 
 var questionSixUser = prompt('Coding bores me (y/n)').toUpperCase();
 console.log('Answer: ' + questionSixUser);
-
-var questionOneAnswer = 'Y';
-var questionTwoAnswer = 'N';
-var questionThreeAnswer = 'Y';
-var questionFourAnswer = 'Y';
-var questionFiveAnswer = 'Y';
 var questionSixAnswer = 'N';
 
-if (questionOneUser === questionOneAnswer) {
+if (questionSixUser === questionSixAnswer) {
   alert('Great job! You guessed correctly!');
 }
 else {

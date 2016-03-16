e7cf4c80f1312a210ce540b4afa1a324900ac5e2
Stefanie Hansen <stefuhnee@gmail.com>
2016-03-16 09:10:44 -0700

    JS file with welcome message

---
+++ app.js
@@ -0,0 +1,6 @@
+var welcomeName = prompt('Hello, what is your name?');
+console.log('User name: ' + welcomeName);
+
+alert('Welcome to my webpage, ' + welcomeName + '! I\'m going to ask you a few questions to help you learn more about me! Please type y or n to answer.');
+
+var questionOneAnswer = prompt('')

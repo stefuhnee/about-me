74335baf9a6510f3e2a6b2d12839f06b1f1f0c9d
Stefanie Hansen <stefuhnee@gmail.com>
2016-03-16 09:52:16 -0700

    Broke up if/else statement into two statements to make it clearer.

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ console.log('User name: ' + welcomeName);
 
 alert('Welcome to my webpage, ' + welcomeName + '! I\'m going to ask you a few questions to help you learn more about me! Please type y or n to answer.');
 
+/* Asks user to answer and changes answer to uppercase, storing it in the variable questionOneUser */
 var questionOneUser = prompt('My favorite food is sushi. (y/n)').toUpperCase();
 console.log('Answer: ' + questionOneUser);
 
 var questionOneAnswer = 'Y'
 
 if (questionOneUser === 'Y' || 'N') {
-  if (questionOneUser === questionOneAnswer) {
+  break;
+}
+else {
+  alert('Please enter your response is the form of \"y or n\"');
+  break;
+}
+
+if (questionOneUser === questionOneAnswer) {
     alert('That is correct!');
   }
   else {
     alert('Wrong answer!');
   }
-else {
-  alert('Please enter your response is the form of \"y or n\"');
-  questionOneUser;
-  continue;
-}
-}

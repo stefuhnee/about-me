8e360bf6f94a4adbf4402535290e40d4330703cb
Stefanie Hansen <stefuhnee@gmail.com>
2016-03-16 09:26:56 -0700

    Added first question prompt and started working on if statement

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ console.log('User name: ' + welcomeName);
 
 alert('Welcome to my webpage, ' + welcomeName + '! I\'m going to ask you a few questions to help you learn more about me! Please type y or n to answer.');
 
-var questionOneAnswer = prompt('')
+var questionOneUser = prompt('My favorite food is sushi. (y/n)').toUpperCase();
+var questionOneAnswer = 'Y'
+
+if (questionOneUser === 'Y' || 'N') {
+  
+}

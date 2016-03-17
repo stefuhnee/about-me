e1267dea4195d555ad2d988e6970d888054e2a37
Stefanie Hansen <stefuhnee@gmail.com>
2016-03-17 11:50:58 -0700

    Added condition to loop if user doesn't enter y or no. Trying to figure out how to remove the answer from the answer array.

--- app.js
+++ app.js
@@ -138,17 +138,27 @@ while (favoriteGames.length < 5) {
   favoriteGames.push(gameAnswer);
   console.log('Games stored so far: ' + favoriteGames);
 } */
-var myQuestions = ['My favorite food is sushi. (y/n)', 'My favorite color is orange. (y/n)', 'I am a unicorn (y/n)', 'I was born in Seattle. (y/n)', 'I enjoy long walks on the beach. (y/n)', 'Coding bores me (y/n)', 'What is the airspeed velocity of an unladen swallow in mph? You have 4 tries!'];
-var myAnswers = ['Y', 'N', 'Y', 'Y', 'Y', 'N', '24'];
+var numCorrectAnswers = 0;
+var totalAnswers = 0;
+
+var myQuestions = ['My favorite food is sushi. (y/n)', 'My favorite color is orange. (y/n)', 'I am a unicorn (y/n)', 'I was born in Seattle. (y/n)', 'I enjoy long walks on the beach. (y/n)', 'Coding bores me (y/n)'];
+var myAnswers = ['Y', 'N', 'Y', 'Y', 'Y', 'N'];
 var userAnswers = [];
 
-for (var i = 0; i < myQuestions.length(); i++) {
-  myAnswers.push((prompt(myQuestions[i])).toUpperCase());
+for (var i = 0; i < myQuestions.length; i++) {
+  userAnswers.push((prompt(myQuestions[i])).toUpperCase());
+  console.log('User Answers so far: ' + userAnswers);
   totalAnswers++;
   if (userAnswers[i] === myAnswers[i]) {
     numCorrectAnswers++;
     alert('Nice work! You got it right!');
-  } else {
+  } else if (userAnswers[i] !== 'Y' || 'N') {
+    alert('Please enter y or n.');
+    i--;
+    userAnswers.remove
+  }
+  else {
     alert('That is not the right answer :(');
   }
+  console.log('Number of correct answers so far: ' + numCorrectAnswers + ' out of ' + totalAnswers);
 }

3d38fd0e4b4f28520a0b85657293ba4be2f96f0d
Stefanie Hansen <stefuhnee@gmail.com>
2016-03-17 09:16:48 -0700

    First draft of the while/if loop for question 6

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var welcomeName = prompt('Hello, what is your name?');
+/* var welcomeName = prompt('Hello, what is your name?');
 console.log('User name: ' + welcomeName);
 
 alert('Welcome to my webpage, ' + welcomeName + '! I\'m going to ask you a few questions to help you learn more about me! Please type y or n to answer.');
 
-/* Asks user to answer and changes answer to uppercase, storing it in the variable questionOneUser */
+Asks user to answer and changes answer to uppercase, storing it in the variable questionOneUser
 var questionOneUser = prompt('My favorite food is sushi. (y/n)').toUpperCase();
 console.log('Do you think my favorite food is sushi?: ' + questionOneUser);
 var questionOneAnswer = 'Y';
@@ -62,7 +62,7 @@ if (questionFiveUser === questionFiveAnswer) {
   var questionSixUser = prompt('Coding bores me (y/n)').toUpperCase();
   console.log('User Answer: ' + questionSixUser);
   var questionSixAnswer = 'N';
-/*Only executes if the user correctly answers Question 5*/
+/*Only executes if the user correctly answers Question 5
   if (questionSixUser === questionSixAnswer) {
     alert('Great job! You guessed correctly! That\'s the end! You rock!');
   }
@@ -74,4 +74,24 @@ if (questionFiveUser === questionFiveAnswer) {
 else {
   alert('Nope, that\'s not right. That\'s it! No more questions!');
   console.log('Equality evaluated as: ' + (questionFiveUser === questionFiveAnswer));
+} */
+
+var guessAttempt = 0;
+var userVelocityGuess = prompt('What is the airspeed velocity of an unladen swallow in mph? You have 4 tries!');
+
+if (guessAttempt <= 4) {
+  while (userVelocityGuess !== 24) {
+    if (userVelocityGuess < 24) {
+      alert('Sorry, your guess is too low.');
+      guessAttempt += guessAttempt;
+    } else if (isNaN(userVelocityGuess)) {
+      alert('That is not a number! Try again!');
+      userVelocityGuess = prompt('What is the airspeed velocity of an unladen swallow? You have 4 tries!');
+    } else {
+      alert('Sorry, your guess is too high!');
+      guessAttempt += guessAttempt;
+    }
+  }
+} else {
+  alert('You are out of guesses! The correct airspeed velocity of an unladen swallow is 24mph. (source: http://style.org/unladenswallow/)')
 }
